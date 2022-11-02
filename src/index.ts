b9a7d46f96b637cb9dc57ef76e5aee72313fefb9
HayHay <dev@hayhay.dev>
2022-11-02 02:44:13 -0400

    User times

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,33 @@ client.on(Events.InteractionCreate, async (interaction) => {
                 }
             });
         }
+
+        if (interaction.commandName == "time") {
+            const user = interaction.options.get("user")?.user;
+            const timezone = interaction.options.get("timezone")?.value as string;
+            const tzFormat = "hh:mm a z"
+
+            if (!user && !timezone)
+                await interaction.reply({content: "Please provide a user or a timezone!", ephemeral: true});
+
+            if (user) {
+                const userTimezone = await db.user.findUnique({
+                    where: {
+                        id: user.id
+                    }
+                });
+
+                if (userTimezone) {
+                    const time = moment().tz(userTimezone.timezone).format(tzFormat);
+                    await interaction.reply(`The time for <@${user.id}> is ${time} in ${userTimezone.timezone}!`);
+                } else {
+                    await interaction.reply("The user " + user.tag + " has not set their timezone yet!");
+                }
+            } else {
+                const time = moment().tz(timezone).format(tzFormat);
+                await interaction.reply("The time in " + timezone + " is " + time + "!");
+            }
+        }
     } else if (interaction.isAutocomplete()) {
         const tzList = moment.tz.names();
         const maxResults = 20;
